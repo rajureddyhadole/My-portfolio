b353fa61247c46474e6720f5f67fc008fa2735ad
Raju <rajureddyhadole452@gmail.com>
2024-08-29 18:48:24 +0530

    add project cards

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function Contact() {
   return (
     <div>
-      this is a contacts page
+      <Navbar/>
+      <div className='flex flex-col h-[570px] items-center justify-center gap-8 p-4 bg-gray-200'>
+        <h1 className='text-3xl font-semibold'>Contact</h1>
+        <input className='h-8 px-2 rounded w-[400px]' type="text" placeholder='Name...' />
+        <input className='h-8 px-2 rounded w-[400px]' type="email" placeholder='Email...'/>
+        <textarea rows='8' className='px-2 rounded w-[400px]' placeholder='Feedback...'></textarea>
+        <button className='px-8 py-2 bg-yellow-400 rounded hover:bg-yellow-500 active:bg-yellow-400'>Submit</button>
+      </div>
+      <Footer/>
     </div>
   )
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function Projects() {
   return (
-    <div>
-      this is a projects page
+    <div className='pb-12'>
+      <Navbar />
+      <div className='flex flex-col items-center p-4 bg-gray-200'>
+        <h1 className='mb-4 text-3xl font-semibold'>Skills</h1>
+        <ul className='grid grid-cols-3 gap-4'>
+          <li className='px-2 py-1 text-center bg-white border-2 rounded border-slate-600'>HTML</li>
+          <li className='px-2 py-1 text-center bg-white border-2 rounded border-slate-600'>CSS</li>
+          <li className='px-2 py-1 text-center bg-white border-2 rounded border-slate-600'>JavaScript</li>
+          <li className='px-2 py-1 text-center bg-white border-2 rounded border-slate-600'>React js</li>
+          <li className='px-2 py-1 text-center bg-white border-2 rounded border-slate-600'>Tailwind CSS</li>
+        </ul>
+      </div>
+
+      <div className='flex flex-col items-center justify-center gap-4 pt-8 bg-gray-200'>
+        <h1 className='text-3xl font-semibold'>Projects</h1>
+
+        <div className='grid grid-cols-3 gap-4 px-8 py-2'>
+
+          <a href="https://github.com/rajureddyhadole/expense-tracker-using-react">  
+            <div className='relative text-center'>
+              <img className='w-full' src="images/expense-tracker.png" alt="" />
+              <div className='absolute bottom-0 flex items-center justify-center text-white bg-black opacity-80 left-1 right-1 top-44'>
+                Expense tracker using React js and Tailwind CSS
+              </div>
+            </div>
+          </a>
+
+          <a href="https://github.com/rajureddyhadole/todoList-using-react">
+            <div className='relative text-center h-[320px]'>
+              <img className='w-full' src="images/todoList.png" alt="" />
+              <div className='absolute bottom-0 flex items-center justify-center text-white bg-black opacity-80 left-1 right-1 top-44'>
+                Todo List using React js and Tailwind CSS
+              </div>
+            </div>
+          </a>
+
+          <a href="https://github.com/rajureddyhadole/color-picker-app">  
+            <div className='relative text-center h-[320px]'>
+              <img className='object-cover w-full' src="images/color-picker-app.png" alt="" />
+              <div className='absolute bottom-0 left-0 right-0 flex items-center justify-center text-white bg-black opacity-80 top-44'>
+                Color Picker app using React js
+              </div>
+            </div>
+          </a>
+          
+        </div>  
+      </div>
+      <Footer />
     </div>
   )
 }
