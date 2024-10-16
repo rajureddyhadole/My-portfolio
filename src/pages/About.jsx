cf61e834f969d1d824194ba92918690f5deed5bd
Hadole Raju <67407713+rajureddyhadole@users.noreply.github.com>
2024-10-16 17:53:29 +0530

    resolved resume issue

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Footer from '../components/Footer'
+import resume from "../assets/Raju's-resume.pdf"
 
 function About() {
   return (
@@ -10,7 +11,7 @@ function About() {
         <p className='w-[80%] font-normal sm:w-[70%] lg:w-[60%] mt-4 sm:text-lg text-neutral-700'>Hi there! I'm Raju reddy, a recent graduate with a degree in Information Technology from St. Peters Engineering College. Eager to apply my academic knowledge and skills to a dynamic professional environment, I'm seeking opportunities to contribute to innovative projects and collaborate with talented teams.
           I'm particularly interested in Web Development and have a passion for Front-end Development.
           I'm excited to leverage my enthusiasm, problem-solving skills, and dedication to drive positive outcomes in a challenging and rewarding role. Let's connect to discuss how my skills and passion can benefit your team.</p>
-        <a className='px-12 py-3 mt-4 text-lg font-semibold rounded-full bg-gradient-to-r from-orange-600 to-orange-800 text-neutral-900' href='images/My Resume.pdf' download="Raju's resume">Resume</a>
+        <a className='px-12 py-3 mt-4 text-lg font-semibold rounded-full bg-gradient-to-r from-orange-600 to-orange-800 text-neutral-900' href={resume} download="Raju's resume">Resume</a>
       </div>
       <Footer />
     </div>
